162bf989f9d99034f51a362b19c23e341ca5b174
nyccyn <nyccyn@gmail.com>
2015-01-18 19:34:27 +0200

    change name to Bill-Draft

--- app/scripts/services/bill.js
+++ app/scripts/services/bill.js
@@ -2,7 +2,7 @@
 
 app.factory('Bill', function (FIREBASE_URL, $firebase) {
   var ref = new Firebase(FIREBASE_URL);
-  var draftBills = $firebase(ref.child('bills-draft')).$asArray();
+  var draftBills = $firebase(ref.child('Bill-Draft')).$asArray();
   var approvedBills = $firebase(ref.child('bills-approved')).$asArray();
 
   var Bill = {
