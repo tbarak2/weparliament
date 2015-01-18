eb23b6fd5839c1a0c352622ac8ced96f4d9686ac
nyccyn <nyccyn@gmail.com>
2015-01-18 19:35:10 +0200

    change name to Bill-Approved

--- app/scripts/services/bill.js
+++ app/scripts/services/bill.js
@@ -3,7 +3,7 @@
 app.factory('Bill', function (FIREBASE_URL, $firebase) {
   var ref = new Firebase(FIREBASE_URL);
   var draftBills = $firebase(ref.child('Bill-Draft')).$asArray();
-  var approvedBills = $firebase(ref.child('bills-approved')).$asArray();
+  var approvedBills = $firebase(ref.child('Bill-Approved')).$asArray();
 
   var Bill = {
     create: function (bill) {
